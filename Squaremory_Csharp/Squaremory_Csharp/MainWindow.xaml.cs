107e0d3a0dd22bff057625f785cee9bf0cde49a5
yolanda <ysimpson@spartaglobal.com>
2019-02-26 17:29:19 +0000

    score added

--- Squaremory_Csharp/Squaremory_Csharp/MainWindow.xaml.cs
+++ Squaremory_Csharp/Squaremory_Csharp/MainWindow.xaml.cs
@@ -46,30 +46,79 @@ namespace Squaremory_Csharp
                     Random rnd = new Random();
                     int row = rnd.Next(6);
                     int col = rnd.Next(6);
+                    
                     TextBlock btc = new TextBlock();
                     btc.Background = Brushes.Black;
 
                     Grid.SetColumn(btc, col);
                     Grid.SetRow(btc, row);
                     grid.Children.Add(btc);
+                    if(increment == 10)
+                    {
+                        btc.Background = Brushes.White;
+                    }
+                   
                 }
+               
             }
 
         }
         private int increment = 16;
+        
         public void myTimer_Tick(object sender, EventArgs e)
         {
+            TextBlock btc1 = new TextBlock();
             increment--;
             if (increment == 0)
             {
                 cd.Stop();
+                MessageBox.Show("game over");
             }
+            if (increment == 10)
+            {
+                btc1.Visibility = Visibility.Hidden;
+                btn1.Visibility = Visibility.Hidden;
+                btn2.Visibility = Visibility.Hidden;
+                btn6.Visibility = Visibility.Hidden;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Hidden;
+                btn15.Visibility = Visibility.Hidden;
+                btn14.Visibility = Visibility.Hidden;
+                btn16.Visibility = Visibility.Hidden;
+                btn20.Visibility = Visibility.Hidden;
+                btn36.Visibility = Visibility.Hidden;
+                btn5.Visibility = Visibility.Hidden;
+                btn35.Visibility = Visibility.Hidden;
+            }
+           
             txttimer.Text = increment.ToString();
+            
         }
-       
 
+        private void changeBlockColour()
+        {
+           
+           
+        }
 
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+           if (increment == 10)
+                {
+                    btn1.Visibility = Visibility.Hidden;
+                }
 
+            
+        }
+
+        private void Btn15_Click(object sender, RoutedEventArgs e)
+        {
+            if (increment == 10)
+            {
+                btn1.Visibility = Visibility.Hidden;
+            }
+
+        }
     }
 }
       
